61fa25ecdb87b2db94d2fcc5fbed5023cf1d7d5d
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2024-04-03 17:04:54 +0100

    test: RED player has initial score of 0

--- test_tennis.py
+++ test_tennis.py
@@ -7,3 +7,8 @@ class Player:
 def test_player_exists():
   player_1 = Player("Player 1")
   assert player_1.name == "Player 1"
+  
+def test_player_score_0():
+  player_1 = Player("Player 1")
+  assert player_1.score == 0
+  
