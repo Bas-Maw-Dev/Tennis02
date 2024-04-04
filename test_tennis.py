c693075a70bb3db626458378025d79bc74dd519d
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2024-04-04 08:43:49 +0100

    refactor: tidied up the tests, made pytest fixture for the Game to avoid duplication

--- test_tennis.py
+++ test_tennis.py
@@ -25,6 +25,10 @@ def player_1():
 def player_2():
   return Player("Player 2")
 
+@pytest.fixture
+def game(player_1, player_2):
+  return Game(Player(player_1.name), Player(player_2.name))
+
 # Each player can have either of these points in one game 0 15 30 40
 
 def test_player_exists(player_1):
@@ -33,20 +37,17 @@ def test_player_exists(player_1):
 def test_player_score_0(player_1):
   assert player_1.score == 0
   
-def test_players_in_a_game(player_1, player_2):
-  game = Game(player_1, player_2)
+def test_players_in_a_game(game):
   assert game.player_1.name == "Player 1"
   assert game.player_2.name == "Player 2"
   
-def test_both_players_score(player_1, player_2):
-  game = Game(player_1, player_2)
+def test_both_players_score(game):
   game.player_1.score_point()
   game.player_2.score_point()
   assert game.player_1.score == 15
   assert game.player_2.score == 15
   
-def test_players_can_score_30(player_1, player_2):
-  game = Game(player_1, player_2)
+def test_players_can_score_30(game):
   game.player_2.score = 15
   game.player_2.score_point()
   assert game.player_2.score == 30
